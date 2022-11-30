dcc76f4c67b8a5d3f9f75864a224477e0744ca82
Artem Drushchyts <artem.drushchyts@tangiblee.com>
2022-11-30 23:57:30 +0300

    feat: implement copy functionality

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,23 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const dist = path.join(__dirname, 'files-copy')
+const pathToFiles = path.join(__dirname, 'files');
+
 const copy = async () => {
-    // Write your code here 
+
+    await fs.mkdir(dist, (err) => {
+        if (err) throw Error('FS operation failed')
+    })
+
+    await fs.readdir(pathToFiles, (err, files) => {
+        if (err) throw Error('FS operation failed')
+        files.forEach((file) => {
+            fs.copyFile(path.join(pathToFiles, file), path.join(dist, file), () => {})
+        })
+    })
 };
 
 copy();
