77df23a7c1f74fcd1597d184bee4b9ebcbe2f75e
Artem Drushchyts <artem.drushchyts@tangiblee.com>
2022-12-01 00:10:01 +0300

    feat: implement rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,15 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const oldPath = path.join(__dirname, 'files', 'wrongFilename.txt');
+const newPath = path.join(__dirname, 'files', 'properFilename.md');
+
 const rename = async () => {
-    // Write your code here 
+    await fs.rename(oldPath, newPath, (err) => {
+        if (err) throw Error('FS operation failed')
+    });
 };
 
 await rename();
