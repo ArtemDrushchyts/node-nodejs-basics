66881e6bac2bb0bf38993b5914ccac0dd5796455
Artem Drushchyts <artem.drushchyts@tangiblee.com>
2022-12-01 00:34:01 +0300

    feat: implement list fn

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,15 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const pathToFiles = path.join(__dirname, 'files');
+
 const list = async () => {
-    // Write your code here 
+    await fs.readdir(pathToFiles, (err, files) => {
+        if (err) throw Error('FS operation failed')
+        console.table(files)
+    });
 };
 
 await list();
