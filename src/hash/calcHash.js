ad51c76f0858919d73c72b45aa7456246570cab4
Artem Drushchyts <artem.drushchyts@tangiblee.com>
2022-12-04 20:06:55 +0300

    feat: implement convert to hex hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
+import { createHash } from 'crypto';
+import { readFile } from 'fs/promises';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+    try {
+        const content = await readFile(path.join(__dirname, 'files', 'fileToCalculateHashFor.txt'), { encoding: 'utf-8' });
+        const hash = createHash('sha256').update(content).digest('hex');
+        console.log(`Hex hash: ${hash}`);
+    } catch (err) {
+        throw err
+    }
 };
 
 await calculateHash();
