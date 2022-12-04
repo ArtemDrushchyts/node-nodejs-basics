939acd58276a8c6cbee788e752168579e1fb097c
Artem Drushchyts <artem.drushchyts@tangiblee.com>
2022-12-04 20:34:10 +0300

    feat: implement write stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,13 @@
+import { createWriteStream } from 'fs';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+    const writeStream = createWriteStream(path.join(__dirname, 'files', 'fileToWrite.txt'));
+    process.stdin.pipe(writeStream);
 };
 
 await write();
