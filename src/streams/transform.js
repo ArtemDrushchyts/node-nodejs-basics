48005d798e6bec5a5b91f19d68c90e64d6e739f5
Artem Drushchyts <artem.drushchyts@tangiblee.com>
2022-12-04 22:19:04 +0300

    feat: implement transform stream

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,17 @@
+import { EOL } from 'os';
+import { Transform, pipeline } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
-};
+    const transformText = new Transform({
+        transform(chunk, enc, cb) {
+            const inputText = chunk.toString().trim();
+            const revert = inputText.split('').reverse().join('');
+            cb(null, revert + EOL)
+        }
+    });
+    pipeline(process.stdin, transformText, process.stdout, (err) => {
+        console.log(err)
+    });
+}
 
 await transform();
