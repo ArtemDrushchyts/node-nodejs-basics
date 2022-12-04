b449a9941293bfdc1af250a9c3e3b9b92f4c4472
Artem Drushchyts <artem.drushchyts@tangiblee.com>
2022-12-04 18:38:53 +0300

    feat: implement args fn

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+    const withoutPath = process.argv.slice(2);
+    const args = withoutPath.reduce((acc, arg, i, arr) => {
+        if(arr[i+1] && arg.startsWith('--')) {
+            acc.push(`${arg.slice(2)} is ${arr[i+1]}`)
+        }
+        return acc
+    }, []);
+    console.log(args.join(', '));
 };
 
 parseArgs();
