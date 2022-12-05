b14e8be29e79a7e04a44a0db67657db2a7505e94
Artem Drushchyts <artem.drushchyts@tangiblee.com>
2022-12-05 21:42:57 +0300

    feat: implemet clild process fn

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,12 @@
+import { fork } from 'child_process'
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const pathToFile = path.join(dirname(__filename), 'files', 'script.js');
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    fork(pathToFile, args);
 };
 
-spawnChildProcess();
+spawnChildProcess(['arg1', 'arg2', 'arg3']);
